90bfbf90795e016f9c109dd863c73619107f529b
dhruv2590 <dhruvpanwar25@gmail.com>
2024-09-09 07:12:24 +0500

    test api route added and database connected

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDb");
+}).catch((err) => {
+    console.log(err);
+})
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server is up and running at port 3000");
+})
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.use("/api/user", userRouter)
